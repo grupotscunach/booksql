0710a9a5e2b7b8d4aa486e97d1a97d1889b8292d
grupotscunach <150868354+grupotscunach@users.noreply.github.com>
2023-11-14 18:53:22 -0600

    Create cap2datos.sql

---
+++ cap2datos.sql
@@ -0,0 +1,9 @@
+-- Insertar datos en las dos tablas
+insert into TabR1 values ('A1', 'A2');
+insert into TabR1 values ('A3', 'A4');
+
+insert into TabR2 values (1, 2, 3, 4);
+insert into TabR2 values (5, 6, 7, 8);
+insert into TabR2 values (9, 10, 11, 12);
+insert into TabR2 values (13, 14, 15, 16);
+
